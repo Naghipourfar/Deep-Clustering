5609db26b0cd541ca8f03aea4e85c840b3e95c54
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-09-03 20:16:26 +0430

    Fix minor bug in rotation

--- Code/DataGenerator.py
+++ Code/DataGenerator.py
@@ -11,19 +11,20 @@ import matplotlib.pyplot as plt
     Skype: mn7697np
 """
 MY_DPI = 192
+RADIUS = 10
 
 
 def draw_ring(n_points=250):
     xs, ys = [], []
+    r = random.random() * RADIUS
     for i in range(n_points):
-        p = random.random() * 2 * math.pi
-        r = 1 * math.sqrt(0.01)
-        x = math.cos(p) * r
-        y = math.sin(p) * r
+        p = 2 * math.pi * random.random()
+        x = r * math.cos(p)
+        y = r * math.sin(p)
+        x += 1
+        y += 1
         xs.append(x)
         ys.append(y)
-    # plt.plot(xs, ys, 'o')
-    # plt.show()
     return xs, ys
 
 
@@ -36,11 +37,6 @@ def draw_circle(n_points=250):
         y = math.sin(p) * r
         xs.append(x)
         ys.append(y)
-    # plt.close("all")
-    # plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
-    # plt.plot(xs, ys, 'o')
-    # plt.axis("off")
-    # plt.savefig("../Results/image.png")
     return xs, ys
 
 
@@ -65,29 +61,36 @@ def draw_square_ring(n_points=250):
     return xs, ys
 
 
-def rotate_point(xs, ys, degree):
+def rotate_point(xs, ys, degree, origin=(0, 0)):
+    degree = math.radians(degree)
     for idx, x in enumerate(xs):
-        y = xs[idx]
-        degree = math.radians(degree)
-        ox, oy = 1, 1
-        rotated_x = ox + math.cos(degree) * (x - ox) - math.sin(degree) * (y - oy)
-        rotated_y = oy + math.sin(degree) * (x - ox) + math.cos(degree) * (y - oy)
-        xs[idx], ys[idx] = rotated_x, rotated_y
+        y = ys[idx]
+        ox, oy = origin
+        x -= ox
+        y -= oy
+        cos = math.cos(degree)
+        sin = math.sin(degree)
+        rotated_x = cos * x - sin * y
+        rotated_y = sin * x + cos * y
+        xs[idx], ys[idx] = rotated_x + ox, rotated_y + oy
     return xs, ys
 
 
 def plot_shapes(xs, ys):
     plt.close("all")
-    # plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
-    plt.figure(figsize=(15, 10))
+    plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
+    # plt.figure(figsize=(15, 10))
     plt.plot(xs, ys, 'o')
     # plt.axis('off')
+    plt.grid()
+    # plt.xlim((-2, 2))
+    # plt.ylim((-2, 2))
     plt.savefig("../Results/image.png")
 
 
-if __name__ == '__main__':
-    number_of_objects = 1
-    shapes = ['circle']
+def main():
+    number_of_objects = 10
+    shapes = ['square ring', 'ring']
     xs, ys = [], []
     for shape in shapes:
         for i in range(random.randint(1, number_of_objects)):
@@ -98,7 +101,17 @@ if __name__ == '__main__':
                 x, y = draw_square_ring(n_points=300)
             elif shape == "ring":
                 x, y = draw_ring(n_points=300)
-            x, y = rotate_via_numpy(x, y, degree=random.random() * 180)
+            degree = random.random() * 180
+            origin = random.random() * 2 - 1, random.random() * 2 - 1
+            x, y = rotate_point(x, y, degree, origin)
             xs = xs + x
             ys = ys + y
     plot_shapes(xs, ys)
+    return xs, ys
+
+
+if __name__ == '__main__':
+    # x, y = [1], [1]
+    # degree = 45
+    # print(rotate_point(x, y, degree))
+    main()
