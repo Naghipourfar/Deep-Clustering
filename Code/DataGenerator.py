797db340e2a34808d5b6f4ddadef4f0ae3093b3d
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-09-03 14:49:17 +0430

    Draw square

--- Code/DataGenerator.py
+++ Code/DataGenerator.py
@@ -2,7 +2,6 @@ import math
 import random
 
 import matplotlib.pyplot as plt
-import numpy as np
 
 """
     Created by Mohsen Naghipourfar on 9/2/18.
@@ -11,12 +10,12 @@ import numpy as np
     Github: https://github.com/naghipourfar
     Skype: mn7697np
 """
+MY_DPI = 192
 
 
-
-def draw_circle(n_points=500):
+def draw_ring(n_points=250):
     xs, ys = [], []
-    for i in range(0, n_points):
+    for i in range(n_points):
         p = random.random() * 2 * math.pi
         r = 1 * math.sqrt(0.01)
         x = math.cos(p) * r
@@ -25,6 +24,38 @@ def draw_circle(n_points=500):
         ys.append(y)
     plt.plot(xs, ys, 'o')
     plt.show()
+    return xs, ys
 
 
-draw_circle()
+def draw_circle(n_points=250):
+    xs, ys = [], []
+    for i in range(n_points):
+        p = random.random() * 2 * math.pi
+        r = 1 * math.sqrt(random.random())
+        x = math.cos(p) * r
+        y = math.sin(p) * r
+        xs.append(x)
+        ys.append(y)
+    plt.close("all")
+    plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
+    plt.plot(xs, ys, 'o')
+    plt.axis("off")
+    plt.savefig("../Results/image.png")
+    return xs, ys
+
+
+def draw_square_ring(n_points=250):
+    xs, ys = [], []
+    for i in range(n_points):
+        x = random.random()
+        y = random.random()
+        xs.append(x)
+        ys.append(y)
+    plt.close("all")
+    plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
+    plt.plot(xs, ys, 'o')
+    plt.axis('off')
+    plt.savefig("../Results/image.png")
+    return xs, ys
+
+draw_circle(n_points=500)
