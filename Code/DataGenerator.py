ccdadbcdc7d954f13546d9f0afb5699a40599fbc
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-09-04 00:56:52 +0430

    Generate Training Data

--- Code/DataGenerator.py
+++ Code/DataGenerator.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -45,11 +46,13 @@ def draw_circle(n_points=250):
 def draw_square_ring(n_points=250):
     width = random.random() * WIDTH
     std = 0.025
+    np.random.seed(int(n_points * width))
     x = list(np.random.normal(loc=0.0, scale=std, size=[n_points // 4]))
     x += list(np.random.uniform(0.0, width, size=[n_points // 4]))
     x += list(np.random.normal(loc=width, scale=std, size=[n_points // 4]))
     x += list(np.random.uniform(0.0, width, size=[n_points // 4]))
 
+    np.random.seed(int(n_points * width + 1))
     y = list(np.random.uniform(0.0, width, size=[n_points // 4]))
     y += list(np.random.normal(loc=width, scale=std, size=[n_points // 4]))
     y += list(np.random.uniform(0.0, width, size=[n_points // 4]))
@@ -60,6 +63,7 @@ def draw_square_ring(n_points=250):
 
 def rotate_point(xs, ys, degree, origin=(0, 0)):
     degree = math.radians(degree)
+    bias = random.random() * 5
     for idx, x in enumerate(xs):
         y = ys[idx]
         ox, oy = origin
@@ -69,46 +73,49 @@ def rotate_point(xs, ys, degree, origin=(0, 0)):
         sin = math.sin(degree)
         rotated_x = cos * x - sin * y
         rotated_y = sin * x + cos * y
-        xs[idx], ys[idx] = rotated_x + ox, rotated_y + oy
+        xs[idx], ys[idx] = rotated_x + ox + bias, rotated_y + oy + bias
     return xs, ys
 
 
-def plot_shapes(xs, ys):
+def plot_shapes(xs, ys, idx=0):
+    result_path = "../Results/train/" + "image-" + str(idx) + ".png"
     # plt.close("all")
     # plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
     # plt.figure(figsize=(15, 10))
     # plt.plot(xs, ys, 'o')
-    # plt.axis('off')
+    plt.axis('off')
     # plt.grid()
-    plt.legend()
+    # plt.legend()
     # plt.xlim((-2, 2))
     # plt.ylim((-2, 2))
-    plt.savefig("../Results/image.png")
+    plt.savefig(result_path, dpi=MY_DPI)
+    data = plt.imread(result_path)[:, :, 0]
+    plt.imsave(result_path, data, cmap=cm.gray)
 
 
 def main():
-    plt.close("all")
-    plt.figure(figsize=(15, 10))
-    number_of_objects = 1
-    shapes = ['square ring']
-    xs, ys = [], []
-    for shape in shapes:
-        for i in range(random.randint(1, number_of_objects)):
-            x, y = [], []
-            if shape == "circle":
-                x, y = draw_circle(n_points=300)
-            elif shape == "square ring":
-                x, y = draw_square_ring(n_points=300)
-            elif shape == "ring":
-                x, y = draw_ring(n_points=300)
-            degree = random.random() * 180
-            # origin = random.random() * 2 - 1, random.random() * 2 - 1
-            # x, y = rotate_point(x, y, degree, origin=(0, 0))
-            xs = xs + x
-            ys = ys + y
-            plt.plot(x, y, 'o', label=shape)
-    plot_shapes(xs, ys)
-    return xs, ys
+    for idx in range(100):
+        plt.close("all")
+        # plt.figure(figsize=(15, 10))
+        plt.figure(figsize=(512 / MY_DPI, 512 / MY_DPI), dpi=MY_DPI)
+        number_of_objects = 5
+        shapes = ['square ring', 'ring']
+        xs, ys = [], []
+        for shape in shapes:
+            for i in range(random.randint(1, number_of_objects)):
+                x, y = [], []
+                if shape == "circle":
+                    x, y = draw_circle(n_points=300)
+                elif shape == "square ring":
+                    x, y = draw_square_ring(n_points=300)
+                elif shape == "ring":
+                    x, y = draw_ring(n_points=300)
+                degree = random.random() * 180
+                x, y = rotate_point(x, y, degree, origin=(0, 0))
+                xs = xs + x
+                ys = ys + y
+        plt.plot(xs, ys, 'o')
+        plot_shapes(xs, ys, idx)
 
 
 if __name__ == '__main__':
