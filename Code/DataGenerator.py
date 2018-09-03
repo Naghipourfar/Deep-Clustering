0f3e7a6f183860034308cd639fb78d9e32fda134
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-09-03 11:09:48 +0430

    Draw circle

--- Code/DataGenerator.py
+++ Code/DataGenerator.py
@@ -1,13 +1,8 @@
-import numpy as np
-import pandas as pd
+import math
+import random
 
-import tensorflow as tf
 import matplotlib.pyplot as plt
-import keras
-
-from keras.layers import Input, Dense, Dropout, BatchNormalization
-from keras.models import Model
-from keras.callbacks import History, CSVLogger
+import numpy as np
 
 """
     Created by Mohsen Naghipourfar on 9/2/18.
@@ -16,3 +11,20 @@ from keras.callbacks import History, CSVLogger
     Github: https://github.com/naghipourfar
     Skype: mn7697np
 """
+
+
+
+def draw_circle(n_points=500):
+    xs, ys = [], []
+    for i in range(0, n_points):
+        p = random.random() * 2 * math.pi
+        r = 1 * math.sqrt(0.01)
+        x = math.cos(p) * r
+        y = math.sin(p) * r
+        xs.append(x)
+        ys.append(y)
+    plt.plot(xs, ys, 'o')
+    plt.show()
+
+
+draw_circle()
