624757e3f89432e41eff4d661c15bea8ef4051ed
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-09-14 22:16:56 +0430

    Add file with classical clustering algorithms

---
+++ Code/ClassicalClustering.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import keras
+
+from keras.layers import Input, Dense, Dropout, BatchNormalization
+from keras.models import Model
+from keras.callbacks import History, CSVLogger
+
+"""
+    Created by Mohsen Naghipourfar on 9/15/18.
+    Email : mn7697np@gmail.com or naghipourfar@ce.sharif.edu
+    Website: http://ce.sharif.edu/~naghipourfar
+    Github: https://github.com/naghipourfar
+    Skype: mn7697np
+"""
+
+def dbscan():
+    pass
+
+def hdbscan():
+    pass
+
+def model_based():
+    pass
+
+def k_means(k=10):
+    pass
+
+def k_medoids(k=10):
+    pass
+
+def gaussian_mixture_models(k=10):
+    pass
